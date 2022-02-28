4741a563766858caa94e7131aaaa16f476c5df65
TEERAWAT PRAROM <teewat@yahoo.com>
2022-02-28 13:39:30 +1100

    added welcome text

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::HomeController < Api::AppController
     def first_page
-        render json: { success: "true" }
+        render json: { success: "true", message: "Welcome to simply order API services" }
     end
 end
